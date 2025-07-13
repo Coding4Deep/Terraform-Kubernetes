69dbec5b6857b36efecfdb0c79338074ed1dd95e
Ubuntu <ubuntu@ip-172-31-86-224.ec2.internal>
2025-07-13 15:11:38 +0000

    adding terra code for vpc peeringt

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "requester_access_key" {
+  type = string 
+}
+variable "requester_secret_key" {
+  type = string 
+}
+
+
+variable "acceptor_access_key" {
+  type = string 
+}
+variable "acceptor_secret_key" {
+  type = string 
+}
+
+variable "acceptor_vpc_name" {
+    type = string
+    default = "k8s-project-vpc"
+}
+
+variable "acceptor_account_id" {
+    type = string
+    default = "595263721303"
+}
